48abe83c7407d777701b92f790b3e31fea4fefe4
mzgk <mzgkworks@gmail.com>
2015-12-29 08:28:28 +0900

    Initial Commit

---
+++ Expense/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Expense
+//
+//  Created by 水垣岳志 on 2015/12/29.
+//  Copyright © 2015年 mzgkworks. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
